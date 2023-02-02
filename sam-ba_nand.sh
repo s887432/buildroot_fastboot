6a188adf789442f8915e75fbad820a6d7d33c1a4
Patrick Lin <patrick.cclin@gmail.com>
2023-02-02 13:10:03 +0800

    Add files via upload

---
+++ sam-ba_nand.sh
@@ -0,0 +1,12 @@
+sam-ba -p usb -b sam9x60-ek -a nandflash -c erase
+sam-ba -p usb -b sam9x60-ek -a nandflash -c writeboot:boot.bin
+sam-ba -p usb -b sam9x60-ek -a nandflash -c write:logo.bmp:0x40000
+sam-ba -p usb -b sam9x60-ek -a nandflash -c write:at91-sam9x60ek.dtb:0x180000
+sam-ba -p usb -b sam9x60-ek -a nandflash -c write:zImage:0x200000
+sam-ba -p usb -b sam9x60-ek -a nandflash -c write:rootfs.ubi:0x800000
+sam-ba -p usb -b sam9x60-ek -a bootconfig -c writecfg:bscr:EMULATION_ENABLED
+sam-ba -p usb -b sam9x60-ek -a bootconfig -c resetemul
+sam-ba -p usb -b sam9x60-ek -a bootconfig -c refreshcfg:emul
+sam-ba -p usb -b sam9x60-ek -a bootconfig -c refreshcfg:emul
+sam-ba -p usb -b sam9x60-ek -a bootconfig -c writecfg:bcp-emul:NFC_IOSET1
+sam-ba -p usb -b sam9x60-ek -a bootconfig -c lockcfg:bcp-emul
